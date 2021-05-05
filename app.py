0df188359be0822bbd4e848eed4ed8a4f3e84a01
Maxwell DeMaio <maxdman321@gmail.com>
2021-05-05 14:26:38 -0400

    Added ability to like/retweet tweets

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ load_dotenv()
 auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET_KEY"))
 auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_SECRET"))
 
+# Rate limit= true: allows us to wait 15 minutes before retrying request
 api = tweepy.API(auth, wait_on_rate_limit=True)
 user = api.me()
 
-print(user)
+# Gather search terms for #100DaysOfLanguage
+search = "LanguageLearning"
+numOfTweets = 500
+
+for tweet in tweepy.Cursor(api.search, search).items(numOfTweets):
+    try:
+        tweet.favorite()
+        print("Tweet liked")
+        tweet.retweet()
+        print("Tweet retweeted")
+        time.sleep(10)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
