6e1eb02c83e7161d7a3efb15a86a9bfc0f8b0663
Maxwell DeMaio <maxdman321@gmail.com>
2021-05-06 15:17:38 -0400

    Added log for others to track progress

---
+++ generateLog.py
@@ -0,0 +1,8 @@
+def write_100days():
+    logFile = open("100DayLog.md", "w+")
+    for x in range(1, 101):
+        logFile.write(f"### Day {x}: \n**Today's Progress:** \n\n**Thoughts:**\n\n")
+    logFile.close()
+
+if __name__ == "__main__":
+    write_100days()
