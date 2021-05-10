c71470fc59b4c1652a6d831c1b1a61ef66b17ded
Maxwell DeMaio <maxdman321@gmail.com>
2021-05-10 17:15:09 -0400

    Added error break if rate limit met

--- app.py
+++ app.py
@@ -39,6 +39,9 @@ def main():
         # Basic error handling - will print out why retweet failed to terminal
         except tweepy.TweepError as e:
             print(e.reason, "Tweet id: " + str(tweet.id))
+            if e.message[0]['code'] == 185:
+                print("Rate limit met, ending program")
+                break
 
         except StopIteration:
             print("Stopping...")
