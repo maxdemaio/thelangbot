e85704c6dce0f7607194ccd02b93072140a6e227
Maxwell DeMaio <maxdman321@gmail.com>
2021-05-11 11:35:18 -0400

    Revised tweeting policy

--- bot.py
+++ bot.py
@@ -14,26 +14,17 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
 def main():
-    """Like and retweet the most recent 100DaysOfLanguage related tweets"""
+    """Like and retweet the most recent 100DaysOfLanguage/langtwt related tweets"""
     
     query = "#langtwt OR langtwt OR #100DaysOfLanguage OR 100daysoflanguage -filter:retweets -result_type:recent"
-    newRetweets = 0 
 
-    for tweet in tweepy.Cursor(api.search, q=query).items():
-        # Breakpoint of 16 new retweets reached
-        # 300 tweets / 3 hours limit --> 100 tweets / 1 hour
-        # Server pings ever 10 minutes so we can do ~16 tweets every interval
-        if newRetweets == 16:
-            break
-
+    for tweet in tweepy.Cursor(api.search, q=query).items(limit=16):
         try:
             # Retweet post
             print("\nRetweet Bot found tweet by @" +
                 tweet.user.screen_name + ". " + "Attempting to retweet...")
             tweet.retweet()
             print("Tweet retweeted!")
-            # Increment newly found retweet number
-            newRetweets += 1
             time.sleep(5)
 
         # Basic error handling - will print out why retweet failed to terminal
@@ -47,7 +38,6 @@ def main():
             print("Stopping...")
             break
     
-    print("Retweeted", newRetweets, "language tweets")
     
 
 if __name__ == "__main__":
