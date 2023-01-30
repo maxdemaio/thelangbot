f5de5e742139e5e12a3e17ca687fc310f0d7e427
maxdemaio <maxdman321@gmail.com>
2023-01-29 21:13:11 -0500

    add since and split into funcs

--- app/bot.py
+++ app/bot.py
@@ -1,53 +1,57 @@
-import tweepy
+import os
 import json
-from datetime import datetime, timedelta
+import tweepy
 
-# Set up your Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+def main():
+    # Get Twitter API credentials from environment variables
+    consumer_key = os.environ["CONSUMER_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_token = os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
-# Authenticate with the Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+    # Authenticate with Twitter API
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, tweet_mode='extended')
 
-# Load the banned and supporter JSON files
-with open('data/banned.json', 'r') as file:
-    banned_data = json.load(file)
-with open('data/supporter.json', 'r') as file:
-    supporter_data = json.load(file)
+    # Open banned and supporter JSON files
+    with open("banned.json", "r") as f:
+        banned_ids = json.load(f)
+    with open("supporters.json", "r") as f:
+        supporter_ids = json.load(f)
 
-# Search for tweets with the #langtwt hashtag
-now = datetime.now()
-last_10_min = now - timedelta(minutes=10)
-tweets = tweepy.Cursor(api.search_tweets,
-                       q="#langtwt",
-                       lang="en",
-                       since_id=last_10_min.strftime("%Y-%m-%d"),
-                       tweet_mode='extended').items(100)
+    # Open last_seen_id JSON file
+    with open("last_seen_id.json", "r") as f:
+        last_seen_id = json.load(f)
 
-# Keep track of the number of tweets from each user
-user_tweet_count = {}
+    # Get tweets
+    tweets = get_tweets(api, last_seen_id)
 
-for tweet in tweets:
-    # Don't retweet if the user is banned
-    if tweet.user.id in banned_data['banned']:
-        continue
-    # Keep track of the number of tweets from this user
-    if tweet.user.id in user_tweet_count:
-        user_tweet_count[tweet.user.id] += 1
-    else:
-        user_tweet_count[tweet.user.id] = 1
-    # Only retweet 2 tweets maximum from one user
-    if user_tweet_count[tweet.user.id] > 2:
-        continue
-    # Retweet the tweet
-    try:
-        if tweet.user.id in supporter_data['supporters']:
+    # Retweet tweets
+    retweet(api, tweets, banned_ids, supporter_ids)
+
+    # Update last_seen_id
+    with open("last_seen_id.json", "w") as f:
+        json.dump(tweets[0].id, f)
+
+def get_tweets(api, last_seen_id):
+    query = "langtwt OR #langtwt"
+    tweets = api.search(q=query, since_id=last_seen_id, tweet_mode='extended')
+    return tweets
+
+def retweet(api, tweets, banned_ids, supporter_ids):
+    frequency = {}
+    for tweet in tweets:
+        if tweet.user.id in banned_ids:
+            continue
+        if tweet.user.id in supporter_ids:
             api.create_favorite(tweet.id)
+        if tweet.user.id not in frequency:
+            frequency[tweet.user.id] = 0
+        if frequency[tweet.user.id] >= 2:
+            continue
         api.retweet(tweet.id)
-        print(f"Retweeted tweet from {tweet.user.screen_name}")
-    except tweepy.TweepError as e:
-        print(f"Failed to retweet tweet from {tweet.user.screen_name}: {e}")
+        frequency[tweet.user.id] += 1
+
+if __name__ == "__main__":
+    main()
